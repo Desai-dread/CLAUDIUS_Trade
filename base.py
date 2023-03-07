6dddce0f1308baaf0f67489d64eb3287b7223189
Alcea <maks.komendant@yandex.ru>
2023-03-07 14:52:58 +0300

    Net with cards

--- base.py
+++ base.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask import render_template, request
+from jinja2 import *
 
 app = Flask(__name__)
 
+cards = ["Купон 1", "Купон 2", "Купон 3", "Купон 4"]
+src = "static/image.jpg"
 @app.route('/')
 def trade():
-    return render_template("trade.html")
+    return render_template("trade.html", src=src, cards=cards)
 
     
